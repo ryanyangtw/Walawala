63c9c1ea3a8319b218b69bab6bded0cd62679018
Ryan <hiphubryan@gmail.com>
2014-10-20 08:28:47 +0800

    add dependent: :destroy to association

--- app/models/category.rb
+++ app/models/category.rb
@@ -14,10 +14,10 @@ class Category < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   
   #many to mant
-  has_many :program_categories
+  has_many :program_categories, dependent: :destroy
   has_many :programs, through: :program_categories
 
-  has_many :user_categories
+  has_many :user_categories, dependent: :destroy
   has_many :subscriber , through: :user_categories, source: :user
 
   validates :title, :presence => true
