82596da1879edeeb5c216c86c48c7ad7e5b461ff
Ryan <hiphubryan@gmail.com>
2014-11-03 00:03:27 +0800

    Remove branch in production.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :keep_releases, 5
 
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/settings.yml}
 
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 # Default branch is :master
